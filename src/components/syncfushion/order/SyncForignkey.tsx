e18d1b74b09da6037fc1c57019d387e6e794e206
Hero Developers <134581766+Herosoftware01@users.noreply.github.com>
2026-03-23 11:36:52 +0530

    added forign key

---
+++ src/components/syncfushion/order/SyncForignkey.tsx
@@ -0,0 +1,95 @@
+import * as React from "react";
+import { useEffect, useState, useRef } from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Filter,
+  Inject,
+  Edit,
+  Sort,
+  ForeignKey,
+  Toolbar
+} from "@syncfusion/ej2-react-grids";
+
+function ForeignKeyColumn() {
+  const gridRef = useRef(null);
+
+  interface OrderRow {
+    mainimagepath : string;
+  }
+
+  const [orderData, setOrderData] = useState<OrderRow[]>([]);
+  const [rgbData, setRgbData] = useState([]);
+
+    const imageTemplate = (props: OrderRow) => {
+      const src = props?.mainimagepath
+    ? props.mainimagepath
+    : "/placeholder.png";
+      
+      return (
+        <img
+      src={src}
+      width={50}
+      height={50}
+      style={{ display: "block" }}
+      onError={(e: any) => {
+        e.target.src = "/placeholder.png";
+      }}
+      alt="img"
+    />
+      );
+    };
+
+  const toolbarOptions = ["Add", "Edit", "Delete", "Update", "Cancel"];
+  const validationRules = { required: true };
+
+  // 🔥 Fetch APIs
+  useEffect(() => {
+    fetch("https://app.herofashion.com/order_panda/")
+      .then((res) => res.json())
+      .then((data) => {
+        setOrderData(data);
+      });
+
+    fetch("https://app.herofashion.com/PrintRgb/")
+      .then((res) => res.json())
+      .then((data) => {
+        setRgbData(data);
+      });
+  }, []);
+
+  return (
+    <div className="control-pane">
+      <div className="control-section">
+        <GridComponent
+          dataSource={orderData}
+          ref={gridRef}
+          allowFiltering={true}
+          allowSorting={true}
+          editSettings={{
+            allowEditing: true,
+            allowDeleting: true,
+            allowAdding: true
+          }}
+          filterSettings={{ type: "Menu" }}
+          toolbar={toolbarOptions}
+        >
+          <ColumnsDirective>
+            {/* Primary Key */}
+            <ColumnDirective field="jobno_oms" headerText="Job No" width="150" isPrimaryKey={true} validationRules={validationRules} />
+            {/* 🔗 Foreign Key Column */}
+            <ColumnDirective field="jobno_oms" headerText="RGB Job" width="180" foreignKeyField="jobno_joint" foreignKeyValue="jobno_joint" dataSource={rgbData} />
+            <ColumnDirective headerText="Image" width="150" template={imageTemplate} />
+            <ColumnDirective field="buyer" headerText="Buyer" width="150" />
+            <ColumnDirective field="style" headerText="Style" width="150" />
+          </ColumnsDirective>
+
+          <Inject services={[Filter, Edit, Sort, ForeignKey, Toolbar]} />
+        </GridComponent>
+      </div>
+    </div>
+  );  
+}
+
+export default ForeignKeyColumn;
